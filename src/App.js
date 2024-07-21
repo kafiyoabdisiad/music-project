302a1a3b64de60a082addbefa2b7130bdec1e475
kaphio <kafiyoabdisiad@gmail.com>
2024-07-21 19:51:14 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import {useState, useEffect} from 'react';
+import Player from './components/Player/Player';
+
+const App = () => {
+  const [songs, setSongs] = useState([
+    {
+      title: "Forget me too ft. Halsey",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song-1.jpg",
+      src: "./music/on-n-on.mp3"
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: "./music/somebody-new.mp3"
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3",
+      img_src: "./images/song-3.jpg",
+      src: "./music/on-n-on.mp3"
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 4",
+      img_src: "./images/song-4.jpg",
+      src: "./music/somebody-new.mp3"
+    }
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    // Example of fetching songs from an API (currently commented out)
+    // const fetchSongs = async () => {
+    //   const response = await fetch('YOUR_API_ENDPOINT');
+    //   const fetchedSongs = await response.json();
+    //   setSongs(fetchedSongs);
+    // };
+
+    // fetchSongs();
+
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
+  return (
+    <div className="App">
+      <Player 
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
+      />
+    </div>
+  );
+}
+
+export default App;
